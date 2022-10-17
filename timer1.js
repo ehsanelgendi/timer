077986f67d1630853ad65b791dab17d85fa6807a
Ehsan Elgendi <ehsan.elgendi@gmail.com>
2022-10-17 19:18:13 +0000

    adding timer1

---
+++ timer1.js
@@ -0,0 +1,10 @@
+let timers = process.argv.slice(2);
+
+for (let timer of timers) {
+  timer = parseInt(timer);
+  if ((typeof timer === 'number' && timer > 0)) {
+    setTimeout(() => {
+      process.stdout.write('.');
+    }, timer * 1000);
+  } 
+}
